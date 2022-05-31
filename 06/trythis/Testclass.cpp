d40044b262717fe0c94ef86ecea11435756981ab
Thiago Araújo <thiago.sky25@gmail.com>
2022-05-31 10:53:55 -0300

    06 added drill, review and trythis

---
+++ 06/trythis/Testclass.cpp
@@ -0,0 +1,20 @@
+/*
+This is what we call object oriented? ;)
+*/
+
+#include <iostream>
+using namespace std;
+
+class cars{
+    public:
+        string brand;
+        string model;
+        int year = 2022;
+};
+
+int main(){
+    cars bmw; // create an object
+    bmw.brand = "BMW";
+    cout<<bmw.year;
+    return 0;
+}
