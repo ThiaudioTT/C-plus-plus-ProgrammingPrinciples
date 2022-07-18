0acd050d5fc5a41304693febf8cee1422fd74997
ThiaudioTT <thiago.sky25@gmail.com>
2022-07-18 11:30:40 -0300

    edited 2

--- 09/exercises/02.cpp
+++ 09/exercises/02.cpp
@@ -21,9 +21,11 @@ output print  will print the name and age X
 
 sort() will sort the age and name vector
 
+Note: the program wont check if already exists one name in it. This is one problem.
 */
 
 #include <iostream>
+#include <algorithm>
 #include <vector>
 
 class Name_pairs
@@ -48,18 +50,58 @@ public:
 int main()
 {
     Name_pairs names;
+    std::cout << "Enter all names: ";
     names.read_names();
+
+    std::cout << "\nNow, enter all ages for that names in order: ";
     names.read_ages();
+
+    std::cout << "\nPrinting 1" << std::endl;
+    names.print(1);
+
+    std::cout<< "\nSorting...";
+    names.sort();
+
+    std::cout << "\nPrinting again: " << std::endl;
+    names.print(1);
 }
 
 void Name_pairs::read_names() {
     std::string s;
-    while(std::cin >> s) // ctrl + d to stop
+    while(std::cin >> s)
+    {
+        if(s == "stop")
+            return;
         name.push_back(s);
+    }
 }
 
 void Name_pairs::read_ages() {
     double n;
-    while(std::cin >> n)
+    for(int i = 0; i < name.size(); i++)
+    {
+        std::cin >> n;
         age.push_back(n);
+    }
+}
+
+void Name_pairs::sort() {
+    std::vector<std::string> Sname = name;
+    std::sort(Sname.begin(), Sname.end());
+
+    std::vector<double> Sage = age;
+
+    for(int i = 0; i < Sname.size(); i++)
+    {
+        for(int j = 0; j < name.size(); j++)
+        {
+            if(Sname[i] == name[j])
+            {
+                Sage[i] = age[j];
+            }
+        }
+    }
+
+    name = Sname;
+    age = Sage;
 }
